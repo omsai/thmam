485b20869f92c55476495e186877f8422202724c
Chaoran Hu <huchaoran@live.com>
2017-07-03 18:18:27 -0400

    write total 5 diff fit functions to compare which is better, no result until now, but believe 3 and 5 will be better, because they do not use gradient

--- fitMovResHun.R
+++ fitMovResHun.R
@@ -1,25 +1,24 @@
 Rcpp::sourceCpp("/Users/ChaoranHu/Desktop/thmam/thmam_hyper.cpp")
 
+## use optim L-BFGS-B
 fitMovResHun1 <- function(data, start,
                           optim.control = list(),
                           integrControl = integr.control()) {
     if (!is.matrix(data)) data <- as.matrix(data)
     dinc <- apply(data, 2, diff)
     integrControl <- unlist(integrControl)
-
-    foo <- function (p) {nllk_fwd_ths(c(4,.5,.1,1,p), dinc, integrControl)}
     
-    fit <- optim(start, foo, method = "L-BFGS-B",
-                 lower = 0.5, upper = 0.9,
+    fit <- optim(par = start, fn = nllk_fwd_ths, data = dinc,
+                 integrControl = integrControl,
+                 method = "L-BFGS-B",
+                 lower = rep(0.0001, 5),
+                 upper = c(Inf, Inf, Inf, Inf, 0.9999),
                  control = optim.control)
-    
-    list(estimate    = fit$par,
-         loglik      = -fit$value,
-         convergence = fit$convergence,
-         counts = fit$counts
-         )
+
+    fit
 }
 
+## use BB::spg
 fitMovResHun2 <- function(data, start,
                           optim.control = list(),
                           integrControl = integr.control()) {
@@ -36,9 +35,57 @@ fitMovResHun2 <- function(data, start,
     fit
 }
 
+## use minqa::bobyqa
+fitMovResHun3 <- function(data, start, lower, upper,
+                          optim.control = list(),
+                          integrControl = integr.control()) {
+    if (!is.matrix(data)) data <- as.matrix(data)
+    dinc <- apply(data, 2, diff)
+    integrControl <- unlist(integrControl)
 
+    fit <- minqa::bobyqa(par = start, fn = nllk_fwd_ths, data = dinc,
+                         integrControl = integrControl,
+                         lower = lower,
+                         upper = upper,
+                         control = optim.control)
 
+    fit
+}
 
+## use lbfgsb3::lbfgsb3
+fitMovResHun4 <- function(data, start,
+                          optim.control = list(),
+                          integrControl = integr.control()) {
+    if (!is.matrix(data)) data <- as.matrix(data)
+    dinc <- apply(data, 2, diff)
+    integrControl <- unlist(integrControl)
+
+    fit <- lbfgsb3::lbfgsb3(prm = start, fn = nllk_fwd_ths, data = dinc,
+                            integrControl = integrControl,
+                            lower = rep(0.0001, 5),
+                            upper = c(Inf, Inf, Inf, Inf, 0.9999),
+                            control = optim.control)
+
+    fit
+}
+
+## use nlopt::cobyla
+fitMovResHun5 <- function(data, start, lower, upper,
+                          integrControl = integr.control()) {
+    if (!is.matrix(data)) data <- as.matrix(data)
+    dinc <- apply(data, 2, diff)
+    integrControl <- unlist(integrControl)
+
+    fit <- nloptr::nloptr(x0 = start, eval_f = nllk_fwd_ths,
+                          data = dinc,
+                          integrControl = integrControl,
+                          lb = lower,
+                          ub = upper,
+                          opts = list("algorithm"   = "NLOPT_LN_COBYLA",
+                                      "print_level" = 3))
+
+    fit
+}
 
 
 
--- simulths.R
+++ simulths.R
@@ -5,10 +5,10 @@ lam0 <- 4
 lam1 <- .5
 lam2 <- .1
 p <- .8
-sigma <- 1
+sigma <- 25
 
-grid <- seq(0, 8, length.out = 10)
+grid <- seq(0, 200, length.out = 11)
 
 data <- rMovResHun(grid, lam0, lam1, lam2, sigma, p, "m")
 
-fitMovResHun2(data, c(lam0, lam1, lam2, sigma, p))
+fitMovResHun3(data, c(lam0, lam1, lam2, sigma, p), lower = c(3,.4,.05,20,.5), upper = c(6,.6,.2,27,.9), optim.control = list(iprint = 4))
