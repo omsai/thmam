5fe599ed7023d997280c5d2a872ac0728cec8e41
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-07-10 14:09:29 -0400

    BUG: Use clusterApply instead of clusterCall
    Other changes:
    
    - Refactor to use argparser.
    
    - Add installation and usage documentation.

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # README #
 
+Three states statistical modeling of animal movements (Moving-Resting-Hunting process).
+
+### Installation ###
+
+You need GNU Scientific Library (GSL) installed.
+On ubuntu you can install GSL using `sudo aptitude install libgsl0-dev`
+
+Then install the required R packages:
+
+``` R
+install.packages(c("argparser", "RcppGSL", "nloptr"))
+```
+
+To run the parallel simulation using the `--parallel` option,
+ou also need an MPI library installed.
+On ubuntu you can install openmpi using `sudo aptitude install libopenmpi-dev openmpi-bin`.
+Then install the R packages:
+
+``` R
+install.packages(c("snow", "Rmpi"))
+```
+
+### Usage ###
+
+The `simulths.R` script generates simulated data and fits the model.
+You can run it with:
+
+``` bash
+# Explanation of the input arguments
+Rscript simulths.R --help
+```
+
+``` bash
+# Run simulation on HPC cluster using MPI and the SLURM scheduler
+Rscript simulths.R --parallel
+```
+
 ### Files Description ###
 
 * `coga2dim.cpp` : Calculate coga2dim.
--- simulths.R
+++ simulths.R
@@ -1,36 +1,50 @@
 options(echo = TRUE)
-args <- commandArgs(trailingOnly = TRUE)
+library(methods)                   # Silence argparser loading message
+library(argparser)
+parser <- arg_parser("Test animal movement model using simulated data")
+parser <- add_argument(parser, "--replicates",
+                       help = "Number of replicates to generate to fit the model",
+                       default = 100)
+parser <- add_argument(parser, "--parallel",
+                       help = "Run using MPI",
+                       flag = TRUE)
+args <- parse_args(parser)
+args
 
-simulcore1 <- function() {
-    simul1 <-  function(){
-        source("fitMovResHun.R")
-        source("rMovResHun.R")
+simul1 <-  function(replicate_number = NULL) {
+    source("fitMovResHun.R")
+    source("rMovResHun.R")
 
-        lam0 <- 4
-        lam1 <- .5
-        lam2 <- .1
-        p <- .8
-        sigma <- 25
+    lam0 <- 4
+    lam1 <- .5
+    lam2 <- .1
+    p <- .8
+    sigma <- 25
 
-        grid <- seq(0, 2000, length.out = 101)
+    grid <- seq(0, 2000, length.out = 101)
 
-        data <- rMovResHun(grid, lam0, lam1, lam2, sigma, p, "m")
+    data <- rMovResHun(grid, lam0, lam1, lam2, sigma, p, "m")
 
-        fit <- fitMovResHun5(data,c(lam0, lam1, lam2, sigma, p),
-                             lower = c(0.001, 0.001, 0.001, 1, 0.001),
-                             upper = c(10, 10, 50, ,50, 0.999))
-        fit$solution
-    }
-    
-    replicate(5, simul1())
+    fit <- fitMovResHun5(data,c(lam0, lam1, lam2, sigma, p),
+                         lower = c(0.001, 0.001, 0.001, 1, 0.001),
+                         upper = c(10, 10, 50, ,50, 0.999))
+    fit$solution
 }
 
-if (args == "parallel") {
+if (args$parallel) {
     library(parallel)
     library(snow)
-    cl <- makeCluster(Sys.getenv()["SLURM_NTASKS"], type = "MPI")
+
+    nslaves <- Sys.getenv("SLURM_NTASKS",
+                          unset = parallel::detectCores())
+    nslaves
+    cl <- makeCluster(nslaves, type = "MPI")
+    cl
     clusterSetupRNG(cl, seed = c(1,2,3,4,5,6))
-    result <- clusterCall(cl, simulcore1)
+    result <- clusterApply(cl, 1:args$replicates, simul1)
     save(result, file =  "hpc.Rdata")
     stopCluster(cl)
+} else {
+    result <- replicate(args$replicates, simul1(), simplify = FALSE)
+    save(result, file =  "hpc.Rdata")
 }
